b3a8a5b5d6a8d641ffa6f1c51e1f205d3853272a
Lena Vu <lenavu@Lenas-MacBook-Pro.local>
2021-05-19 18:52:04 +0300

    webpack

---
+++ module1/fe/src/index.ts
@@ -0,0 +1,11 @@
+import './styles.scss'
+
+const a: number = 1012
+const b = 12
+
+const add = (a: any, b: any) => a + b
+console.log(add(a, b))
+
+let greetings: string = 'Hello, World!'
+
+console.log(greetings)

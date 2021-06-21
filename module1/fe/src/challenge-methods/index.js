08f9600309878f3842919ad7ec48413b1c5dd2d5
haodangthi <mywayrabota2@gmail.com>
2021-06-21 18:22:20 +0300

    deleted js

--- module1/fe/src/challenge-methods/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.updateTask = void 0;
-var update_task_1 = require("./update-task");
-Object.defineProperty(exports, "updateTask", { enumerable: true, get: function () { return update_task_1.updateTask; } });
-//# sourceMappingURL=index.js.map
